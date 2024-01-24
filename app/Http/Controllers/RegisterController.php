e92e89595cab564be3f31f2465e2b0443dd90450
Gabriel Ravelo <ravelogabriel2@gmail.com>
2024-01-24 18:14:20 -0400

    registro, login, autenticacion, muro

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -15,12 +18,35 @@ class RegisterController extends Controller
     public function store(Request $request)
     {
         // dd($request);
+
+        // Modificar el Request
+        $request->request->add(['username' => Str::slug($request->username)]);
         // Validacion
         $this->validate($request, [
             'name' => 'required|max:30',
             'username' => 'required|unique:users|min:3|max:20',
             'email' => 'required|unique:users|email|max:60',
-            'password' => 'required'
+            'password' => 'required|confirmed|min:6'
         ]);
+
+        // dd('Creando usuario...');
+        User::create([
+            'name'=>$request->name,
+            'username'=> $request->username,
+            'email'=>$request->email,
+            'password'=> Hash::make($request->password)
+        ]);
+
+        // Autenticar a un usuario
+        // auth()->attempt([
+        //     'email' => $request->email,
+        //     'password' => $request->password
+        // ]);
+
+        // Otra forma de autenticar
+        auth()->attempt($request->only('email', 'password'));
+
+        // Redireccionar al usuario
+        return redirect()->route('posts.index');
     }
 }
