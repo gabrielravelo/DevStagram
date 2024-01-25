c9375dafa3895f1ace5f7b88d1885a546bff1967
gabrielravelo <g.ravelo.menssajero@gmail.com>
2024-01-25 17:19:51 -0400

    mostrando publicaciones en el muro + paginacion

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,8 +15,11 @@ class PostController extends Controller
 
     public function index(User $user)
     {
+        $posts = Post::where('user_id', $user->id)->paginate(20);
+
         return view('dashboard', [
-            'user'=>$user
+            'user'=>$user,
+            'posts'=>$posts
         ]);
     }
 
@@ -47,6 +50,14 @@ class PostController extends Controller
         // $post->imagen = $request->imagen;
         // $post->user_id = auth()->user()->id;
         // $post->save();
+
+        // Tercera forma
+        // $request->user()->posts()->create([
+        //     'titulo'=>$request->titulo,
+        //     'descripcion'=>$request->descripcion,
+        //     'imagen'=>$request->imagen,
+        //     'user_id'=>auth()->user()->id
+        // ]);
 
         return redirect()->route('posts.index', auth()->user()->username);
     }
